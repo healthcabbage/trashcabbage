75f5309c519c5fe5ffccd57d064b04ffbeeb2d43
위대하신 Su님 <suyoung998@naver.com>
2019-04-03 14:59:10 +0900

    conwa
    Gyaaaaaa

---
+++ Assets/Script/CharacterMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    Rigidbody2D rigid;
+    Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if(Input.GetAxisRaw ("Horizontal")<0){
+            moveVelocity = Vector3.left;
+        }
+        
+        else if(Input.GetAxisRaw ("Horizontal")>0){
+            moveVelocity = Vector3.right;
+        }
+        
+        else if(Input.GetAxisRaw ("Vertical")<0){
+            moveVelocity = Vector3.down;
+        }
+        
+        else if(Input.GetAxisRaw ("Vertical")>0){
+            moveVelocity = Vector3.up;
+        }
+
+        transform.position += moveVelocity * moveSpeed * Time.deltaTime;
+    }
+}
