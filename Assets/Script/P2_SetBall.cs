2dd72cffa69e9d377bf4049d8db5b3f5cdf56c95
위대하신 Su님 <suyoung998@naver.com>
2019-05-28 17:38:05 +0900

    a

--- Assets/Script/P2_SetBall.cs
+++ Assets/Script/P2_SetBall.cs
@@ -4,31 +4,37 @@ using UnityEngine;
 
 public class P2_SetBall : MonoBehaviour
 {
+    GameObject ShootBall;
     public Vector3 pos; //마우스 오른쪽 클릭시 공이 세팅될 위치
+    private bool usingcol = false; //충돌시 트루반환
 
-    private bool ballTF = false;
-    private bool posTF = false; //쏠 공이 자리에 세팅되어 있는지 검사
+
 
     void Start()
     {
-        pos = new Vector3(8.59f, 5.67f);
+        pos = new Vector3(8.59f, -5.47f);
     }
 
     void Update()
-    {
-        if (Input.GetMouseButtonDown(1) && posTF == false)  //마우스 오른쪽 버튼 클릭 시
+    {      
+        if (Input.GetMouseButtonDown(1) && usingcol == true)   //마우스 오른쪽 버튼 클릭 시
         {
-            print("충돌");
-           // other.transform.position = pos;
-            posTF = true;
+            if (usingcol == true) 
+            {
+                ShootBall.transform.position = pos;
+                usingcol = false;
+            }
+
+            //  gameObject.GetComponent<P2_SetBall>().enabled = false; //스프라이트종료코드
         }
     }
 
     void OnTriggerEnter2D(Collider2D other) //other : Collider 사용시 모든 콜리더와 충돌비교
     {
-        if (other.CompareTag("passage2"))
+        if (other.gameObject.tag == "Red" || other.gameObject.tag == "Blue" || other.gameObject.tag == "Green" || other.gameObject.tag == "Orange" || other.gameObject.tag == "Pink")
         {
-            ballTF = true;
+            ShootBall = other.gameObject;
+            usingcol = true;
         }
     }
 }
